3b82a128e4ef27e625894320e6f363a8d650376b
eber <eber@maisdados.com.br>
2019-12-18 09:08:02 -0300

    Dynamic types lesson.

---
+++ certificacao-csharp-pt1/Aula7 - lidar com tipos dinâmicos/1 - string, var, object e dynamic/depois/UsandoDynamic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class UsandoDynamic : IAulaItem
+    {
+        public void Executar()
+        {
+            String vText = "Certigficação C#";
+            var vSecondText = "Certigficação C#";
+            object vThirdText = "Certigficação C#";
+
+            vText.ToUpper();
+            vSecondText.ToUpper();
+            vThirdText = ((String)vThirdText).ToUpper();
+
+            //vText = 123;
+            //vSecondText = 123;
+            vThirdText = 123;
+
+            vThirdText = (int)vThirdText + 4;
+
+            Console.WriteLine(vThirdText);
+
+            //dynamic é o único tipo que é definido em tempo de execução
+            dynamic vFourthText = "Certificação C#";
+            vFourthText = vFourthText.ToUpper();
+
+            Console.WriteLine(vFourthText);
+
+            vFourthText = 123;
+
+
+        }
+    }
+}
---
+++ certificacao-csharp-pt1/Aula7 - lidar com tipos dinâmicos/2 - Conversões entre dynamic e outros/depois/ConversoesDeDynamic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class ConversoesDeDynamic : IAulaItem
+    {
+        public void Executar()
+        {
+            dynamic vOne = 7;
+            dynamic vTwo = DateTime.Now;
+            dynamic vThree = new List<int>() { 132, 456, 789 };
+
+            int vFirst = vOne;
+            DateTime vSecond = vTwo;
+            List<int> vThird = vThree;
+
+            vThird = vTwo;//Conversão com erro em tempo de execução
+        }
+    }
+}
